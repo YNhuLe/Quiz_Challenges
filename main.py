1a20dcfac411b9f8499af221e98bb59d02800913
YNhuLe <Lenhuy10011996@gmal.com>
2023-11-29 11:40:30 -0500

    Adding loop to ask use if they want to try more

--- main.py
+++ main.py
@@ -3,16 +3,23 @@ from data import question_data
 from quiz_brain import QuizBrain
 
 question_bank = []
+is_continue = True
+while is_continue:
+    print("\n")
+    keep_going = input("Good try!!! Do you want to try the game one more time!!")
+    if keep_going.lower() == "yes":
+        for question in question_data:
+            question_text = question["question"]
+            question_answer = question["correct_answer"]
+            new_question = Question(question_text, question_answer)
+            question_bank.append(new_question)
 
-for question in question_data:
-    question_text = question["question"]
-    question_answer = question["correct_answer"]
-    new_question = Question(question_text, question_answer)
-    question_bank.append(new_question)
+        quiz = QuizBrain(question_bank)
+        while quiz.still_have_question():
+            quiz.next_question()
 
-quiz = QuizBrain(question_bank)
-while quiz.still_have_question():
-    quiz.next_question()
+        print("You've completed the quiz")
+        print(f"Your final score is: {quiz.score}/{quiz.question_number}")
 
-print("You've completed the quiz")
-print(f"Your final score is: {quiz.score}/{quiz.question_number}")
+    else:
+        is_continue = False
